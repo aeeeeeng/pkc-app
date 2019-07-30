b0f88119a6ad35df133c52cabf5a0184c95e78b2
syahril ardi <aeeeeeng@gmail.com>
2019-07-30 09:48:16 +0700

    change bredcumb

--- application/views/layouts/breadcrumb.php
+++ application/views/layouts/breadcrumb.php
@@ -1,11 +1,10 @@
 <ol class="breadcrumb">
+    <li> <a href="<?= base_url(); ?>">Home</a> </li>
     <?php foreach ($this->uri->segments as $segment): ?>
         <?php 
             $url = substr($this->uri->uri_string, 0, strpos($this->uri->uri_string, $segment)) . $segment;
             $is_active =  $url == $this->uri->uri_string;
         ?>
-
-        <li> <a href="<?= base_url(); ?>">Home</a> </li>
         <li class=" <?php echo $is_active ? 'active': '' ?>">
             <?php if($is_active): ?>
                 <?php echo ucwords(str_replace('_', ' ', $segment)); ?>
