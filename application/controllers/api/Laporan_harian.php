44ed9ce72afabbd7c67adea9ae26b2d4b8134d11
syahril ardi <aeeeeeng@gmail.com>
2019-08-24 13:59:20 +0700

    complete upload sftp laporan harian

--- application/controllers/api/Laporan_harian.php
+++ application/controllers/api/Laporan_harian.php
@@ -72,12 +72,24 @@ class Laporan_harian extends PKCC_Controller {
     public function download()
     {
         $this->is_GET();
-        
-        $path = $this->input->get('path');
-        $real_path = base64_decode($path);
-        // echo $real_path;
         $this->load->helper('download');
-        force_download($real_path, NULL);
+        $path = $this->input->get('path');
+        $name = $this->input->get('name');
+        $real_path = base64_decode($path);
+        $real_name = base64_decode($name);
+        $files = $this->files_m->where('file_name', $real_name)->first();
+        $download_sftp_file = $this->general->download_sftp($files->file_path, $real_path);
+        $new_real_path = str_replace("./", "", $real_path);
+        $path_delete = FCPATH.$new_real_path;
+        if($download_sftp_file) {
+            $file_content = file_get_contents($path_delete);
+            if(file_exists($path_delete)){
+                unlink($path_delete);
+            }
+            force_download($real_name, $file_content);
+        } else {
+            show_404();
+        }
     }
 
     public function store()
@@ -100,11 +112,15 @@ class Laporan_harian extends PKCC_Controller {
                 $lh_day = str_pad($this->input->post('lh_day'), 2, "0", STR_PAD_LEFT);
                 $upload = $this->files->upload_file(date("Y/m/d/H/i/s").'-laporan_harian-'.$lh_day.'-'.$lh_month.'-'.$lh_year, './upload/documents/laporan_harian');   
                 if ($upload['success']) {
+
                     $message = $upload['message'];
+                    $target_add = 'laporan_harian/'.$message['file_name'];
+                    $sftp_file = $this->general->move_sftp($message, $target_add);
+
                     $file = [
                         'file_name' => $message['file_name'],
-                        'file_path' => $message['full_path'],
-                        'file_download_path' => base_url('api/laporan_harian/download?path='.base64_encode('./upload/documents/laporan_harian/'.$message['file_name'])),
+                        'file_path' => $sftp_file,
+                        'file_download_path' => base_url('api/laporan_harian/download?path='.base64_encode('./upload/documents/laporan_harian/'.$message['file_name']).'&name='.base64_encode($message['file_name'])),
                         'file_ext' => $message['file_ext']
                     ];
                     $this->transaction->start();
@@ -188,11 +204,12 @@ class Laporan_harian extends PKCC_Controller {
                 unlink($old_file->file_path);
                 $upload = $this->files->upload_file(date("Y/m/d/H/i/s").'-laporan_harian-'.$lh_day.'-'.$lh_month.'-'.$lh_year, './upload/documents/laporan_harian');
                 $message = $upload['message'];
-                
+                $target_add = 'laporan_harian/'.$message['file_name'];
+                $sftp_file = $this->general->move_sftp($message, $target_add);
                 $new_file = [
                     'file_name' => $message['file_name'],
-                    'file_path' => $message['full_path'],
-                    'file_download_path' => base_url('api/laporan_harian/download?path='.base64_encode('./upload/documents/laporan_harian/'.$message['file_name'])),
+                    'file_path' => $sftp_file,
+                    'file_download_path' => base_url('api/laporan_harian/download?path='.base64_encode('./upload/documents/laporan_harian/'.$message['file_name']).'&name='.base64_encode($message['file_name'])),
                     'file_ext' => $message['file_ext']
                 ];
                 $this->files_m->update($old_file->id, $new_file);
@@ -221,6 +238,10 @@ class Laporan_harian extends PKCC_Controller {
         $this->is_DELETE();
         $this->auth->user();
         try {
+            $lh = $this->laporan_harian_m->find($id);
+            $file = $this->files_m->find($lh->file_id);
+            $this->general->delete_sftp($file->file_path);
+            $this->files_m->delete($file->id);
             $this->laporan_harian_m->delete($id);
             $this->status = 200;
             $this->result = ['success' => TRUE, 'message' => 'berhasil di hapus'];
